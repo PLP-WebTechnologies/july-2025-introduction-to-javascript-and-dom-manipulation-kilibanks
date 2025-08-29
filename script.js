5ac52657c12eae353ef556417c77038f25484b2b
kilibanks <wanmun3456@gmail.com>
2025-08-29 18:27:56 +0300

    worked on DOM properties as assigned

---
+++ script.js
@@ -0,0 +1,37 @@
+//variables and conditionals
+let car= "mecedez Benz";
+let student= {name: "John", age: 20, gender: "male"};
+console.log(student);
+console.log(car);
+
+// custom functions
+    function greet(){
+        console.log("hello everyone this is a function")
+    }
+    greet();
+
+    const subtract= (a,b)=> a-b;
+    console.log(subtract(4,2));
+
+
+// loopholes 
+
+for(int i=4, i<=10, i++){
+    console.log("iterate: ${i}"); // output 1,2,3,4,5,6,7,8,9,10
+}
+
+let x=5
+    while (x >=10) {
+        console.log("count: ${x}");
+        x++;
+    }}
+
+
+// DOM interactions
+const h1= document.getElementById("h1");
+console.log(h1.textContent);
+
+const p= document.getElementById("p");
+p.style.color ="orange";
+p.style.borderColor ="blue";
+
